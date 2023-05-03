5ab518b8eb55ecd0d3e68f5812c72fc5fcd02a59
Alfonso6z <alfonso6z.w@gmail.com>
2023-05-03 13:24:09 -0600

    documantación

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,16 @@ const Scene = require('Scene');
 const Patches = require('Patches');
 const Persistence = require('Persistence');
 const FaceTracking = require('FaceTracking');
+const Reactive = require('Reactive');
+
+/* Estas son constantes usadas en el juego para establecer la tolerancia de colisión
+detección (`tolerancia`), la posición inicial de los objetos (`inicioPx`), la
+separación entre objetos (`sepPx`), y la probabilidad de que aparezca un premio
+*/
+const tolerancia = 0.03;
+const inicioPx = -0.075;
+const sepPx = 0.05;
+const paramProbabilidad = 0.3;
 
 // mensajes en consola
 export const Diagnostics = require('Diagnostics');
@@ -11,18 +21,25 @@ export const Diagnostics = require('Diagnostics');
 import * as utl from './utils.js';
 
 (async function () {
-  
-  //* NOTE: Inicio de variables del juego
+  /* NOTE: variables de inicialización para el juego. */
   let tiempo = 30;
   let score = 0;
   let highScore = 0;
   let actualizarScoreText = "";
-
-  // guardar la referencia al localStore
-  const localStorage = Persistence.local;
-
+  
+  //*NOTE: persistencia
+  /* Creando una referencia al objeto de almacenamiento local provisto por la Persistencia
+   módulo en la API JavaScript de Spark AR Studio. Esto permite que el script almacene
+   y recuperar datos localmente en el dispositivo del usuario. */
+  const localStorage = Persistence.local; 
 
   //* NOTE: recupera el score mas alto
+  /* Este bloque de código intenta recuperar la puntuación más alta del almacenamiento local mediante
+  el método `localStorage.get()` proporcionado por el módulo `Persistence` en el
+  API de Spark AR Studio. Primero intenta recuperar la puntuación más alta y registra un
+  mensaje que indica si se recuperó correctamente o no. si un error
+  ocurre durante el proceso de recuperación, registra un mensaje que indica que la puntuación
+  no pudo ser recuperado. */
   try {
     highScore = await localStorage.get('highScore');
     if(highScore){
@@ -33,8 +50,14 @@ import * as utl from './utils.js';
   } catch (error) {
     Diagnostics.log("No se recupero el puntaje");
   }
-
+  
   //* NOTE: recuperar objetos de la scena.
+  /* Este código utiliza la asignación de desestructuración para asignar múltiples variables en
+   una vez de los valores resueltos de una Promesa devuelta por `Promise.all()`. El
+   A Promise se le pasa una serie de promesas que se resuelven en los objetos.
+   representando diferentes elementos en la escena Spark AR Studio. las variables
+   asignados corresponden a estos elementos y se utilizarán más adelante en el
+   código. */
   const [rectanguloTiempo,textScore,textTiempo,textGameOver,
     display,planeEscondite,pulseStar] = await Promise.all([
     Scene.root.findFirst('rectanguloTiempo'),
@@ -47,29 +70,40 @@ import * as utl from './utils.js';
   ]);
   
   //*NOTE: Usu del canva display para optener el tamaño del dispositivo.
-  const display_width = display.width.pinLastValue();
-  const display_height = display.height.pinLastValue();
+  utl.tamanioPantalla(display);
 
   //*NOTE: Que el planeEscondite siga el rostro.
+  /* posición del objeto `planeEscondite`,seguir la posición de la cara del usuario.
+   Está utilizando el módulo `FaceTracking` para rastrear la posición de la cara
+   del usuario y actualizar la posición `x` e `y` de el objeto `planeEscondite`*/
   planeEscondite.transform.x = FaceTracking.face(0).cameraTransform.position.x;
   planeEscondite.transform.y = FaceTracking.face(0).cameraTransform.position.y;
 
-  //*NOTE: Recupera objetos premio 
+  //*NOTE: Recupera objetos premio y castigo 
+  /* Estas líneas de código usan el método `findByPath()` del módulo `Scene`
+  para encontrar todos los objetos en la escena que tienen un patrón de nombre específico. El `**` es
+  un comodín que coincida con cualquier número de niveles en la jerarquía de la escena, y
+  `tecuich*` y `bad*` son patrones que coinciden con cualquier objeto cuyo nombre comience
+  con "tecuich" o "bad", respectivamente. Los objetos resultantes se almacenan en el
+  variables `premios` y `castigos`, que se utilizan más adelante en el código para
+  inicialice las posiciones de los objetos y agregue detección de colisión. */
   const premios = await Scene.root.findByPath("**/tecuich*");
-  //*NOTE: Recupera objetos castigo
   const castigos = await Scene.root.findByPath("**/bad*");
+ 
   //* NOTE: inicia los premios en su lugar
-  utl.iniciarObjetos(premios);
-  utl.iniciarObjetos(castigos);
+  utl.iniciarObjetos(premios,inicioPx,sepPx);
+  utl.iniciarObjetos(castigos,inicioPx,sepPx);
+
+  //* NOTE: Valores iniciales de Score y tiempo 
+  textScore.text = `Score: ${score}`;
+  textTiempo.text = `${tiempo}`; 
 
   //*NOTE: inicializar objetos de la barra de tiempo
-  //! comentar si el tamaño y la posición se realiza en spark studio 
-  utl.tamanioYposicion(rectanguloTiempo,display_width,display_height,80,6,10,8,true);
-  utl.tamanioYposicion(textScore,display_width,display_height,37,6,10,13,true);
-  utl.tamanioYposicion(textTiempo,display_width,display_height,14,6,75,13,true);
-  utl.tamanioYposicion(textGameOver,display_width,display_height,80,30,10,15,true);
-  textScore.text = `Score: ${score}`;
-  textTiempo.text = `${tiempo} s`;
+  //! comentar si el tamaño y la posición se realiza en spark studio
+  utl.tamanioYposicion(rectanguloTiempo,57,6,31,8+2,true);
+  utl.tamanioYposicion(textTiempo,15,9,7,6.5+2,true);
+  utl.tamanioYposicion(textScore,30,9,7,13,true);
+  utl.tamanioYposicion(textGameOver,65,13,11,6,true);
 
   //* NOTE: creacion de la animación para la barra de timepo
   const controladorTiempo = utl.creaControladorTiempo(tiempo);
@@ -81,29 +115,38 @@ import * as utl from './utils.js';
   const animacionTiempo = utl.animacionLineal(controladorTiempo,tiempo,0);
   // actualiza el tiempo en pantalla
   animacionTiempo[1].monitor().subscribe(()=>{
-    textTiempo.text = animacionTiempo[1].round().pinLastValue()+"s";
+    textTiempo.text = `${animacionTiempo[1].round().pinLastValue()}`;
   });
 
   //* NOTE: agregar las coliciones
-  utl.iniciaColiciones(premios,planeEscondite,incrementaScore);
-  utl.iniciaColiciones(castigos,planeEscondite,decrementarScore);
+  utl.iniciaColiciones(premios,planeEscondite,tolerancia,incrementaScore);
+  utl.iniciaColiciones(castigos,planeEscondite,tolerancia,decrementarScore);
 
-  //* NOTE: inicia las animaciones
+  /**
+   * La función "comenzarJuego" inicia un juego ocultando ciertos elementos,
+   * iniciar animaciones para premios y penalizaciones, y configurar un temporizador que
+   * activa una pantalla de fin de juego cuando finaliza.
+   */
   function comenzarJuego(){
     rectanguloTiempo.hidden = false;
-    textScore.hidden = false;
     textTiempo.hidden = false;
     planeEscondite.hidden = false;
+    textScore.hidden = false;
     animacionRectangulo[0].start();
     animacionTiempo[0].start();
     // NOTE: crear animaciónes e iniciarlas para premios y castigos.
-    utl.animacionPremios(premios,castigos,animacionTiempo[1]);
+    utl.animacionPremios(premios,castigos,animacionTiempo[1],paramProbabilidad);
+    
     // si el tiempo termina muestra gameOver
     animacionTiempo[1].eq(0).onOn().subscribe(terminar);
-
   }
 
   // NOTE:Terminar 
+  /**
+   * Esta función oculta varios elementos y muestra un mensaje de fin del juego 
+   * con la puntuación del jugador, y envía un pulso a la escena para indicar 
+   * que el juego termino.
+   */
   async function terminar(){
     rectanguloTiempo.hidden = true;
     textScore.hidden = true;
@@ -128,11 +171,20 @@ import * as utl from './utils.js';
     textGameOver.text = actualizarScoreText;
     textGameOver.hidden = false;
     
-    // TODO: animación final particulas 
-    // Devuelve un EventSource(pulso) que emite un evento vacío una sola vez, tan pronto como sea posible.
-    // Patches.inputs.setPulse('GameOver',Reactive.once())
+    /* `Patches.inputs.setPulse('GameOver',Reactive.once());` está enviando un pulso
+     (o evento) al editor de parches con el nombre "GameOver" y un valor de
+     `Reactive.once()`, que es una señal que emite un solo evento tan pronto como
+     posible. Esto se usa para indicar que el juego ha terminado y activar cualquier
+     parches o scripts relevantes en el editor de parches. */
+    Patches.inputs.setPulse('GameOver',Reactive.once());
   }
 
+
+  /**
+   * La función disminuye la puntuación y oculta un elemento dado, si la puntuación es
+   * mayor que cero decrementa los puntos
+   * @param castigo - El parámetro "castigo" es le objeto que tiene que esconder
+   */
   function decrementarScore(castigo){
     castigo.hidden = true;
     if(score>0){
@@ -141,12 +193,19 @@ import * as utl from './utils.js';
     }
   }
 
+  /**
+   * La función incrementa la puntuación y oculta un elemento dado.
+   * @param premio - El parámetro "premio" es le objeto que tiene que esconder
+   */
   function incrementaScore(premio){
     premio.hidden = true;
     score++;
     textScore.text = `Score: ${score}`;
   }
   
+  
   // * si pulso la pantalla inicia
+  /* Estas líneas de código se suscriben a diferentes pulsos (eventos) y llaman
+   diferentes funciones cuando esos pulsos son activados. */
   pulseStar.subscribe(comenzarJuego);
 })();
--- scripts/utils.js
+++ scripts/utils.js
@@ -1,26 +1,28 @@
-// import {velocidadMax,
-//   velocidadMin,
-//   paramProbabilidad,
-//   tolerancia,
-//   visibilidadEnY,
-//   terminaLaAnimacion,
-//   inicioPx,
-//   sepPx,
-//   Diagnostics,
-// } from './script.js';
-
 const Reactive = require('Reactive');
 const Animation = require('Animation');
 
-export const velocidadMax = 15; 
-export const velocidadMin = 40;
-export const paramProbabilidad = 0.3;
-export const tolerancia = 0.03;
+// velocidad de la animación rango
+const velocidadMax = 15; 
+const velocidadMin = 40;
+
 export const visibilidadEnY = 0.17;
-export const posicionEnY = 0.17;
+export const posicionEnY = 0.16;
 export const terminaLaAnimacion = -0.23;
-export const inicioPx = -0.075;
-export const sepPx = 0.05;
+
+let display_width;
+let display_height;
+
+/**
+ * La función "tamanioPantalla" recupera el ancho y alto de una pantalla.
+ * @param display - El parámetro "display" es el objeto que contiene información 
+ * sobre la pantalla de visualización, como su ancho y alto.Toma este objeto 
+ * como argumento y le asigna los valores de ancho y alto para las variables 
+ * "display_width" y "display_height"
+ */
+export function tamanioPantalla(display){
+  display_width = display.width.pinLastValue();
+  display_height =display.height.pinLastValue();
+}
 
 /**
  * Esta función establece el tamaño de respuesta de un objeto en función del
@@ -31,26 +33,24 @@ export const sepPx = 0.05;
  * @param hRelativo - Altura relativa del objeto que se esta redimencionando 
  * @param objeto - Objeto cuyo tamaño debe establecerse de forma receptiva.
  */
-export function estableceTamanioResponcivo(wDisplay,hDisplay,wRelativo,hRelativo,objeto){
+export function estableceTamanioResponcivo(wRelativo,hRelativo,objeto){
     // tamaño
-    objeto.width = (wDisplay*wRelativo)/100;
-    objeto.height = (hDisplay*hRelativo)/100;
+    objeto.width = (display_width*wRelativo)/100;
+    objeto.height = (display_height*hRelativo)/100;
 }
 
 /**
  * Esta función establece la posición de un objeto en función de la visualización
  * relativa
- * @param wDisplay - El ancho de la pantalla en pixeles
- * @param hDisplay - La altura de la pantalla en pixeles
  * @param wRelativo - El ancho relativo del objeto en porcentaje. Es utilizado para
  * calcular la posición horizontal del objeto en función del ancho de la pantalla
  * @param hRelativo - La altura relativa del objeto en porcentaje. Calcula la posición 
  * verticla del objeto.
  * @param objeto - Objeto cuya posición es calculada
  */
-export function establecePosicionReponcivo(wDisplay,hDisplay,wRelativo,hRelativo,objeto){
+export function establecePosicionReponcivo(wRelativo,hRelativo,objeto){
     // posición
-    objeto.transform.position = Reactive.point((wDisplay*wRelativo)/100,(hDisplay*hRelativo)/100,0);
+    objeto.transform.position = Reactive.point((display_width*wRelativo)/100,(display_height*hRelativo)/100,0);
 }
 
 /**
@@ -61,12 +61,12 @@ export function establecePosicionReponcivo(wDisplay,hDisplay,wRelativo,hRelativo
  * @param sep - Distancia de separación entre los objetos en el array. se utiliza
  * para calcular la posicion de cada objeto.
  */
-export function iniciarObjetos(objArray){
-    var x = inicioPx;
+export function iniciarObjetos(objArray,inicio,sep){
+    var x = inicio;
     objArray.forEach((obj)=>{
       obj.hidden = true;
       obj.transform.position = Reactive.point(x,posicionEnY,0);
-      x+=sepPx;
+      x+=sep;
     });
 }
 
@@ -82,7 +82,7 @@ export function iniciarObjetos(objArray){
  * @returns el valor de la variable `n`, que es el último carácter de la
   * Propiedad `name` de `obj1` analizada como un entero.
  */
-export function colisionDosObj(obj1,obj2,consecuencia){
+export function colisionDosObj(obj1,obj2,tolerancia,consecuencia){
     const n = parseInt((obj1.name[obj1.name.length - 1]));
     const obj2X = obj2.transform.x;
     const obj2Y = obj2.transform.y;
@@ -97,7 +97,21 @@ export function colisionDosObj(obj1,obj2,consecuencia){
 }
 
 
-// TODO: documenar
+
+/**
+ * Esta función crea una animación lineal utilizando un controlador de tiempo y una muestra en
+ * Spark AR Studio.
+ * @param controladorTiempo - Un controlador de tiempo que controla la duración y el tiempo
+ * de la animación.
+ * @param inicio - El valor inicial de la animación. Podría ser un número,
+ * vector, o valor de color según el contexto de la animación.
+ * @param final - El valor final de la animación. Este es el valor que el
+ * la animación llegará al final de su duración.
+ * @returns La función `animacionLineal` devuelve un array que contiene el tiempo
+ * driver(inicia o detiene la animación) y animación(cambia el valor) creados 
+ * usando los parámetros de entrada `controladorTiempo`,
+ * `inicio`, y `final`.
+ */
 export function animacionLineal(controladorTiempo,inicio,final){
     // controlador de tiempo
     const driverTiempo = Animation.timeDriver(controladorTiempo);
@@ -108,25 +122,61 @@ export function animacionLineal(controladorTiempo,inicio,final){
     return [driverTiempo,animacion];
 }
 
-// TODO: documenar
+/**
+ * Esta función crea una animación lineal para un objeto de premio dado entre dos
+ * posiciones especificadas utilizando un controlador de tiempo.
+ * @param premio - El objeto que representa el premio que será animado.
+ * @param inicio - La posición inicial de la animación. es un valor numerico
+ * que representa la posición inicial del objeto del premio.
+ * @param final: el valor final de la animación, que es la posición final de
+ * el objeto "premio".
+ * @retorna el controlador de animación creado por la función `animacionLineal`.
+ */
 function creaAnimacionPremio(premio,inicio,final){
   premio.hidden = false;
-  // FIXME: velocidad
   const driveTiempo = creaControladorTiempo(0,1,false,true,velocidadMax,velocidadMin);
   const animacion = animacionLineal(driveTiempo,inicio,final);
   premio.transform.position = Reactive.point(premio.transform.position.x.pinLastValue(),animacion[1],0);
   return animacion[0];
 }
 
-// TODO: documentar
-export function animacionPremios(premios,castigos,observar){
+
+/**
+ * La funcion animacionPremios anima una lista de premios y castigos con un
+ * probabilidad dada y observación.
+ * @param premios: una matriz de elementos que representan las recompensas que se animarán.
+ * @param castigos - Una matriz de objetos que representan las penas o castigos
+ * asociado a cada premio de la matriz "premios". La matriz "castigos" debe
+ * tener la misma longitud que la matriz "premios".
+ * @param observar - Es un driveTime del tiempo para observar cuando se detandra
+ * la animación.
+ * @param probabilidad - La probabilidad de que ocurra la animación. Donde 0 
+ * significa que la animación nunca ocurrirá y 1 significa la animación siempre 
+ * ocurrirá.
+ */
+export function animacionPremios(premios,castigos,observar,probabilidad){
   premios.forEach((p,i)=>{
-    animarPremios(p,castigos[i],observar);
+    animarPremios(p,castigos[i],observar,probabilidad);
   });
 }
 
-// TODO: documentar
-function animarPremios(premio,castigo,observar){
+
+/**
+ * La función anima un elemento de recompensa o castigo elegido al azar y escucha
+ * para cuando cae, luego continúa la animación si no se ha cumplido el límite de tiempo
+ * alcanzado y elige aleatoriamente otro elemento de recompensa o castigo.
+ * @param premio: una variable que representa el elemento de recompensa que se va a animar.
+ * @param castigo - Este parámetro representa el elemento que se mostrará como un
+ * castigo en la animación.
+ * @param observar - Parece que "observar" es una variable o parámetro que es
+ * se usa para observar o rastrear algún valor o condición. Es probable que sea un Pin o
+ * Observable que se está pasando a la función para monitorear algún aspecto del
+ * animación o lógica del juego.
+ * @param paramProbabilidad - Este parámetro es un valor de probabilidad entre 0 y 1
+ * que determina la probabilidad de que la función elija un "premio" sobre
+ * un "castigo" (castigo) al animar el ítem.
+ */
+function animarPremios(premio,castigo,observar,paramProbabilidad){
   // elige si es premio o castigo 
   if(Math.random()>paramProbabilidad){
    var item = premio;
@@ -135,7 +185,7 @@ function animarPremios(premio,castigo,observar){
   }
 
   // animar el item premio | castigo
-  var ap = creaAnimacionPremio(item,visibilidadEnY,terminaLaAnimacion,observar);
+  var ap = creaAnimacionPremio(item,visibilidadEnY  ,terminaLaAnimacion,observar);
   ap.start();
   // escucha si el premio cae
   premio.transform.position.y.lt(terminaLaAnimacion).onOn().subscribe(()=>{
@@ -172,31 +222,47 @@ function animarPremios(premio,castigo,observar){
   });
 }
 
-// TODO: documentar
-export function iniciaColiciones(premios,colicion,consecuencia){
+
+/**
+ * La función "iniciaColiciones" itera a través de una matriz de "premios" y
+ * comprueba si hay colisiones con otro objeto "colicion" usando una "tolerancia" dada
+ * y aplica una "consecuencia" si se produce una colisión.
+ * @param premios - una serie de objetos que representan los premios o castigo en el
+ * juego
+ * @param colicion - La función utiliza este parámetro para comprobar si hay
+ * colisiones entre los "premios" (premios) y el objeto "colicion".
+ * @param tolerancia - El parámetro de tolerancia en esta función representa la
+ * distancia máxima entre dos objetos antes de que se considere que colisionan.
+ * Si la distancia entre dos objetos es menor o igual a la tolerancia
+ * valor, entonces se considera que están colisionando.
+ * @param consecuencia - El parámetro "consecuencia" es una función que será
+ * ejecutada si se produce una colisión entre dos objetos. Es una función de devolución de llamada.
+ * que se llamará con el objeto premio como argumento.
+ */
+export function iniciaColiciones(premios,colicion,tolerancia,consecuencia){
   premios.forEach((p)=>{
-    colisionDosObj(p,colicion,consecuencia);
+    colisionDosObj(p,colicion,tolerancia,consecuencia);
   });
 }
 
 /**
-  * La función crea un objeto controlador de tiempo con duración personalizable, número
-  * de iteraciones, duplicación y aleatorización opcional.
-  * @param tiempo - La duración del controlador en segundos.
-  * @param [repeticiones=1] - La cantidad de veces que el controlador debe repetir su
-  * animación. Si no se especifica.
-  * @param [espejo=false] - un valor booleano que determina si la animación
-  * debe jugar al revés después de completar un ciclo. Si se establece en verdadero, la animación
-  * reproducirá hacia adelante y luego hacia atrás. Si se establece en falso, la animación solo
-  * jugar hacia adelante.
-  * @param [aleatorio=false] - un valor booleano que determina si la duración
-  * del controlador debe ser aleatorio o no. Si se establece en verdadero, la duración
-  * debe ser un número entero aleatorio entre los valores de los parámetros min y max.
-  * @param [min=0] - El valor mínimo para la generación de tiempo aleatorio. 
-  * @param [max=0] - El valor máximo para la generación de tiempo aleatorio.
-  * @returns La función `creaControladorTiempo` devuelve un objeto con propiedades
-  * `durationMilliseconds`, `loopCount` y `mirror`.
-  */
+ * La función crea un objeto controlador de tiempo con duración personalizable, número
+ * de iteraciones, duplicación y aleatorización opcional.
+ * @param tiempo - La duración del controlador en segundos.
+ * @param [repeticiones=1] - La cantidad de veces que el controlador debe repetir su
+ * animación. Si no se especifica.
+ * @param [espejo=false] - un valor booleano que determina si la animación
+ * debe jugar al revés después de completar un ciclo. Si se establece en verdadero, la animación
+ * reproducirá hacia adelante y luego hacia atrás. Si se establece en falso, la animación solo
+ * jugar hacia adelante.
+ * @param [aleatorio=false] - un valor booleano que determina si la duración
+ * del controlador debe ser aleatorio o no. Si se establece en verdadero, la duración
+ * debe ser un número entero aleatorio entre los valores de los parámetros min y max.
+ * @param [min=0] - El valor mínimo para la generación de tiempo aleatorio. 
+ * @param [max=0] - El valor máximo para la generación de tiempo aleatorio.
+ * @returns La función `creaControladorTiempo` devuelve un objeto con propiedades
+ * `durationMilliseconds`, `loopCount` y `mirror`.
+ */
 export function creaControladorTiempo(tiempo,repeticiones=1,espejo=false,
                                         aleatorio=false,min=0,max=0){
     if(aleatorio || tiempo<0){
@@ -214,23 +280,53 @@ export function creaControladorTiempo(tiempo,repeticiones=1,espejo=false,
 }
 
 /**
-  * La función establece el tamaño de respuesta y la posición de un objeto y 
-  * también puede esconderlo.
-  * @param obj - El objeto que necesita ser redimensionado y reposicionado.
-  * @param wDispaly - El ancho de la pantalla de visualización.
-  * @param hDisplay - La altura de la pantalla donde estará el objeto.
-  * @param wTamanio - tamaño del ancho del objeto
-  * @param hTamanio - El tamaño de la altura del objeto.
-  * @param wPosicion - La posición de ancho del objeto,posición horizontal.
-  * @param hPosicion - La posición de altura del objeto,posición vertical.
-  * @param oculto - Valor booleano que determina si el objeto debe estar oculto.
-  */
-export function tamanioYposicion(obj,wDispaly,hDisplay,wTamanio,hTamanio,wPosicion,hPosicion,oculto){
-  estableceTamanioResponcivo(wDispaly,hDisplay,wTamanio,hTamanio,obj);
-  establecePosicionReponcivo(wDispaly,hDisplay,wPosicion,hPosicion,obj);
+ * La función establece el tamaño de respuesta y la posición de un objeto y 
+ * también puede esconderlo.
+ * @param obj - El objeto que necesita ser redimensionado y reposicionado.
+ * @param wTamanio - tamaño del ancho del objeto
+ * @param hTamanio - El tamaño de la altura del objeto.
+ * @param wPosicion - La posición de ancho del objeto,posición horizontal.
+ * @param hPosicion - La posición de altura del objeto,posición vertical.
+ * @param oculto - Valor booleano que determina si el objeto debe estar oculto.
+ */
+export function tamanioYposicion(obj,wTamanio,hTamanio,wPosicion,hPosicion,oculto){
+  estableceTamanioResponcivo(wTamanio,hTamanio,obj);
+  establecePosicionReponcivo(wPosicion,hPosicion,obj);
   obj.hidden = oculto;
 }
 
+
+/**
+ * La función genera un número entero aleatorio entre un valor mínimo y máximo.
+ * @param min - El valor mínimo del rango desde el cual el entero aleatorio debería
+ * ser generado.
+ * @param max - El valor máximo que puede tomar el entero aleatorio.
+ * @returns La función `aleratorioInt` devuelve un entero aleatorio entre `min`
+ * y valores `max` (inclusive).
+ */
 export function aleratorioInt(min, max) {
     return Math.floor( Math.random() * (max - min) + min);
+}
+
+
+/**
+ * Esta función crea una animación de premioDorado usando una animación lineal
+ * controlador y un controlador de tiempo.
+ * @param dorado: objeto o entidad en la escena que representa un objeto o personaje dorado.
+ * @param animacion - El parámetro animacion es un objeto de Animación Reactiva que
+ * controla el tiempo y la progresión de la animación. Se utiliza para iniciar y
+ * detener la animación, así como suscribirse a los eventos que ocurren durante la
+ * animación.
+ */
+export function doradoAnimacion(dorado,animacion){
+  const controladorTime = creaControladorTiempo(1);
+  const ap = animacionLineal(controladorTime,0.17,-0.23);
+  dorado.transform.position = Reactive.point(dorado.transform.position.x.pinLastValue(),ap[1],0);
+  animacion.lt(16).onOn().subscribe(()=>{
+    ap[0].start();
+    dorado.hidden = false;
+  });
+  dorado.transform.position.y.lt(-0.23).onOn().subscribe(()=>{
+    dorado.hidden = true;
+  })
 }
